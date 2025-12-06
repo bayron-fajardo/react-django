0aa4f301fbaf08d093e1e99c6e44cf0fa22efcc3
Bayron Fajardo <bayronfajardor@gmail.com>
2025-12-06 15:30:47 -0500

    feat: conexion entre el backend-frontend

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coreapi',
     'tasks',
     'rest_framework',
     'corsheaders',
@@ -126,5 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    
+    "http://localhost:5173"
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS' : 'rest_framework.schemas.coreapi.AutoSchema',
+}
